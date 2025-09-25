bff4c39ae329fb993cd5e1812c7d80903adf2769
HaNa <sohana0808@gmail.com>
2025-09-26 00:57:04 +0600

    :rocket: running 2 app

---
+++ apps/backend/src/modules/users/users.controller.ts
@@ -0,0 +1,27 @@
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { UsersService } from './users.service';
+
+@Controller()
+export class UsersController {
+  constructor(private readonly usersService: UsersService) { }
+
+
+
+  @MessagePattern('findAllUsers')
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @MessagePattern('findOneUser')
+  findOne(@Payload() id: number) {
+    return this.usersService.findOne(id);
+  }
+
+
+
+  @MessagePattern('removeUser')
+  remove(@Payload() id: number) {
+    return this.usersService.remove(id);
+  }
+}
